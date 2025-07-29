6e0ef94f77b664811b79a4f8beb373284d85a67f
lifesboy <49264651+lifesboy@users.noreply.github.com>
2025-07-29 22:30:08 +0700

    add test cases for problem4

--- src/problem4/main.ts
+++ src/problem4/main.ts
@@ -12,7 +12,20 @@ function sum_to_n_c(n: number): number {
 
 console.log('Start')
 
-console.assert(sum_to_n_a(0) === 0, 'sum_to_n_a:0 => FAIL')
+console.assert(sum_to_n_a(1) === 1, 'sum_to_n_a:1 => FAIL')
+console.assert(sum_to_n_a(2) === 3, 'sum_to_n_a:2 => FAIL')
+console.assert(sum_to_n_a(5) === 15, 'sum_to_n_a:5 => FAIL')
+console.assert(sum_to_n_a(6) === 21, 'sum_to_n_a:6 => FAIL')
+
+console.assert(sum_to_n_b(1) === 1, 'sum_to_n_a:1 => FAIL')
+console.assert(sum_to_n_b(2) === 3, 'sum_to_n_a:2 => FAIL')
+console.assert(sum_to_n_b(5) === 15, 'sum_to_n_a:5 => FAIL')
+console.assert(sum_to_n_b(6) === 21, 'sum_to_n_a:6 => FAIL')
+
+console.assert(sum_to_n_c(1) === 1, 'sum_to_n_a:1 => FAIL')
+console.assert(sum_to_n_c(2) === 3, 'sum_to_n_a:2 => FAIL')
+console.assert(sum_to_n_c(5) === 15, 'sum_to_n_a:5 => FAIL')
+console.assert(sum_to_n_c(6) === 21, 'sum_to_n_a:6 => FAIL')
 
 
 console.log('Done')
