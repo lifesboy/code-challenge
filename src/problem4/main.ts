40d2d2271ca10692ef466bc571327d6b8f8e4cfc
lifesboy <49264651+lifesboy@users.noreply.github.com>
2025-07-29 22:28:01 +0700

    init problem4 template

---
+++ src/problem4/main.ts
@@ -0,0 +1,18 @@
+function sum_to_n_a(n: number): number {
+    return n
+}
+
+function sum_to_n_b(n: number): number {
+    return n
+}
+
+function sum_to_n_c(n: number): number {
+    return n
+}
+
+console.log('Start')
+
+console.assert(sum_to_n_a(0) === 0, 'sum_to_n_a:0 => FAIL')
+
+
+console.log('Done')
