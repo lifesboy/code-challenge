b431e5aac188ec1e0e130190163acdd911f68280
lifesboy <49264651+lifesboy@users.noreply.github.com>
2025-07-31 08:54:06 +0700

    Separate api service layer

---
+++ src/problem5/domain/codes/index.ts
@@ -0,0 +1,2 @@
+export * as ERROR from './error'
+export * as SUCCESS from './success'
---
+++ src/problem5/domain/codes/success.ts
@@ -0,0 +1,4 @@
+export const CREATED = 'CREATED'
+export const DELETED = 'DELETED'
+export const SUCCESS = 'SUCCESS'
+
